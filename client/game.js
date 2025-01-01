3600c7d17b5bd3c30e0343e635c4f2c2eba99fc0
mircea007 <mirceareb@gmail.com>
2025-01-01 19:29:11 +0200

    small refactor

--- client/game.js
+++ client/game.js
@@ -1,25 +1,42 @@
-let canvas_el = document.getElementById('canvas');
+canvas_el = document.getElementById('canvas');
 // fit screen to body size
 
 const RATIO = 16/9;
-let HEIGHT = 180;
-let WIDTH = 320;
-let need_new_canvas = true;
+HEIGHT = 180;
+WIDTH = 320;
+need_new_canvas = true;
 
-let ctx = canvas_el.getContext('2d');
+ctx = canvas_el.getContext('2d');
 
-let BACKGROUND_COL = '#90cc90';
-let DARK_BACKGROUND_COL = '#85c085'
+BACKGROUND_COL = '#90cc90';
+DARK_BACKGROUND_COL = '#85c085'
 
-let KICK_WIDTH = 3;
-let KICK_COLOR = '#ffffff';
+KICK_WIDTH = 3;
+KICK_COLOR = '#ffffff';
 
-let PITCH_LINE_COLOR = '#eeeeee';
+PITCH_LINE_COLOR = '#eeeeee';
+
+PADDING = 0.1 * HEIGHT;
+PITCH_X_BEGIN = PADDING;
+PITCH_X_END = WIDTH - PADDING;
+
+PITCH_Y_BEGIN = PADDING;
+PITCH_Y_END = HEIGHT - PADDING;
+
+POLE_BIG_Y = HEIGHT * 0.65;
+POLE_SMALL_Y = HEIGHT * 0.35;
+
+LEN_CAREU = HEIGHT * 0.2;
+PADDING_CAREU = HEIGHT * 0.1;
+
+CENTER_RADIUS = HEIGHT * 0.075;
+
+const BANDS = 12;
 
 const NEXT_ROUND_DELAY = 3000;
 const TRANSITION = 200;
 
-let client_server_time_delta = 0; // initialy assume client and server have same clocks
+client_server_time_delta = 0; // initialy assume client and server have same clocks
 
 window.addEventListener("resize", _ => {
   need_new_canvas = true;
@@ -44,34 +61,83 @@ function handle_wh_changes() {
 
 handle_wh_changes();
 
-let FPS = 30;
+FPS = 30;
 
-function draw_scene( {'round_info': round_info, 'bodies': entity_list, 'players': player_list} ){
-  handle_wh_changes();
+function draw_players( player_list ) {
+  //ctx.fillText('Press P for players', 20, HEIGHT - 20);
+  const NAME_PREFIX = 12;
+  const NAME_MARGIN = 10;
+  const TEAM_RAD = 10;
+  const BIG_MARGIN = 7;
 
-  const PADDING = 0.1 * HEIGHT;
-  const PITCH_X_BEGIN = PADDING;
-  const PITCH_X_END = WIDTH - PADDING;
+  const Y_COMP = PITCH_Y_END + BIG_MARGIN;
+  x_blue = PADDING;
+  x_red = WIDTH - PADDING;
+  player_list.forEach( ({'name': name, 'team': team, 'on_pitch': on_pitch}) => {
+    short_name = name;
+    if( name.length > NAME_PREFIX )
+      short_name = name.slice(0, NAME_PREFIX - 2) + '...';
 
-  const PITCH_Y_BEGIN = PADDING;
-  const PITCH_Y_END = HEIGHT - PADDING;
+    text_width = ctx.measureText(short_name).width;
+    comp_width = text_width + 3 * NAME_MARGIN + 2 * TEAM_RAD;
 
-  const POLE_BIG_Y = HEIGHT * 0.65;
-  const POLE_SMALL_Y = HEIGHT * 0.35;
+    x_render = (team == 0) ? x_blue : x_red - comp_width;
+    if( team == 0 )
+      x_blue += (comp_width + BIG_MARGIN);
+    else
+      x_red -= (comp_width + BIG_MARGIN);
 
-  const LEN_CAREU = HEIGHT * 0.2;
-  const PADDING_CAREU = HEIGHT * 0.1;
+    // background
+    ctx.fillStyle = (on_pitch) ? '#75a075' : 'rgba(0, 0, 0, 0.3)';
+    ctx.fillRect(x_render, Y_COMP, comp_width, 2 * NAME_MARGIN + 2 * TEAM_RAD);
 
-  const CENTER_RADIUS = HEIGHT * 0.075;
+    // team circle
+    ctx.fillStyle = (team == 0) ? 'blue' : 'red';
+    ctx.beginPath();
+    ctx.arc(x_render + NAME_MARGIN + TEAM_RAD, Y_COMP + NAME_MARGIN + TEAM_RAD, TEAM_RAD, 0, 2 * Math.PI);
+    ctx.fill();
 
-  const BANDS = 12;
+    // player name
+    ctx.font = "15px Courier New, monospace";
+    ctx.fillStyle = "#ffffff";
+    ctx.fillText(short_name, x_render + 2 * NAME_MARGIN + 2 * TEAM_RAD, Y_COMP + 2.5 * NAME_MARGIN);
+  } );
+}
 
+function draw_bodies( entity_list ) {
+  entity_list.forEach( (corp) => {
+    x = corp['x'] * HEIGHT;
+    y = corp['y'] * HEIGHT;
+    R = corp['R'] * HEIGHT;
+
+    ctx.fillStyle = corp['color'];
+    ctx.beginPath();
+    ctx.arc(x, y, R, 0, 2 * Math.PI);
+    ctx.fill();
+
+    if( corp['kicking'] ){
+      ctx.lineWidth = KICK_WIDTH;
+      ctx.strokeStyle = KICK_COLOR;
+      ctx.stroke();
+    }
+
+    const nick = corp['name'];
+    if( nick ){
+      ctx.font = "15px Courier New, monospace";
+      ctx.fillStyle = "#ffffff";
+      text_info = ctx.measureText(nick);
+      ctx.fillText(nick, x - text_info.width / 2, y - R - 10);
+    }
+  });
+}
+
+function draw_pitch() {
   ctx.fillStyle = BACKGROUND_COL;
   ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
   // dark bands on the pitch, even bands are dark, odd ones are light
   ctx.fillStyle = DARK_BACKGROUND_COL;
-  for( let i = 0; i < BANDS; i += 2 )
+  for( i = 0; i < BANDS; i += 2 )
     ctx.fillRect(PITCH_X_BEGIN + (PITCH_X_END - PITCH_X_BEGIN) * i / BANDS, PITCH_Y_BEGIN, (PITCH_X_END - PITCH_X_BEGIN) / BANDS, PITCH_Y_END - PITCH_Y_BEGIN);
 
   ctx.strokeStyle = PITCH_LINE_COLOR;
@@ -101,6 +167,27 @@ function draw_scene( {'round_info': round_info, 'bodies': entity_list, 'players'
   ctx.beginPath();
   ctx.arc(WIDTH / 2, HEIGHT / 2, CENTER_RADIUS, 0, 2 * Math.PI);
   ctx.stroke();
+}
+
+function draw_scene( {'round_info': round_info, 'bodies': entity_list, 'players': player_list} ){
+  handle_wh_changes();
+
+  PADDING = 0.1 * HEIGHT;
+  PITCH_X_BEGIN = PADDING;
+  PITCH_X_END = WIDTH - PADDING;
+
+  PITCH_Y_BEGIN = PADDING;
+  PITCH_Y_END = HEIGHT - PADDING;
+
+  POLE_BIG_Y = HEIGHT * 0.65;
+  POLE_SMALL_Y = HEIGHT * 0.35;
+
+  LEN_CAREU = HEIGHT * 0.2;
+  PADDING_CAREU = HEIGHT * 0.1;
+
+  CENTER_RADIUS = HEIGHT * 0.075;
+
+  draw_pitch();
 
   document.body.style.backgroundColor = '';
 
@@ -124,8 +211,8 @@ function draw_scene( {'round_info': round_info, 'bodies': entity_list, 'players'
 
     ctx.fillStyle = "#ffffff";
     if( round_info.msg ){
-      let lines = round_info.msg.split(/\n/);
-      let nlines = lines.length;
+      lines = round_info.msg.split(/\n/);
+      nlines = lines.length;
 
       if( nlines >= 1 )
         ctx.fillText(lines[0], WIDTH / 2 - ctx.measureText(lines[0]).width / 2, PADDING / 2 + FONT_HEIGHT / 2);
@@ -140,30 +227,8 @@ function draw_scene( {'round_info': round_info, 'bodies': entity_list, 'players'
       document.body.style.backgroundColor = 'red';
   }
 
-  entity_list.forEach( (corp) => {
-    x = corp['x'] * HEIGHT;
-    y = corp['y'] * HEIGHT;
-    R = corp['R'] * HEIGHT;
-
-    ctx.fillStyle = corp['color'];
-    ctx.beginPath();
-    ctx.arc(x, y, R, 0, 2 * Math.PI);
-    ctx.fill();
-
-    if( corp['kicking'] ){
-      ctx.lineWidth = KICK_WIDTH;
-      ctx.strokeStyle = KICK_COLOR;
-      ctx.stroke();
-    }
-
-    const nick = corp['name'];
-    if( nick ){
-      ctx.font = "15px Courier New, monospace";
-      ctx.fillStyle = "#ffffff";
-      let text_info = ctx.measureText(nick);
-      ctx.fillText(nick, x - text_info.width / 2, y - R - 10);
-    }
-  });
+  //if( Math.floor(+(new Date()) / 100) % 100 == 0 )
+  //  console.log(round_info);
 
   ctx.font = "15px Courier New, monospace";
   ctx.fillStyle = "#ffffff";
@@ -172,50 +237,11 @@ function draw_scene( {'round_info': round_info, 'bodies': entity_list, 'players'
     20, 20
   );
 
-  //if( Math.floor(+(new Date()) / 100) % 100 == 0 )
-  //  console.log(round_info);
-
   ctx.fillText('' + round_info.in_play, WIDTH - 60, 20);
   ctx.fillText('FPS = ' + FPS.toFixed(1), 20, 40);
 
-  //ctx.fillText('Press P for players', 20, HEIGHT - 20);
-  const NAME_PREFIX = 12;
-  const NAME_MARGIN = 10;
-  const TEAM_RAD = 10;
-  const BIG_MARGIN = 7;
-
-  const Y_COMP = PITCH_Y_END + BIG_MARGIN;
-  let x_blue = PADDING;
-  let x_red = WIDTH - PADDING;
-  player_list.forEach( ({'name': name, 'team': team, 'on_pitch': on_pitch}) => {
-    let short_name = name;
-    if( name.length > NAME_PREFIX )
-      short_name = name.slice(0, NAME_PREFIX - 2) + '...';
-
-    let text_width = ctx.measureText(short_name).width;
-    let comp_width = text_width + 3 * NAME_MARGIN + 2 * TEAM_RAD;
-
-    let x_render = (team == 0) ? x_blue : x_red - comp_width;
-    if( team == 0 )
-      x_blue += (comp_width + BIG_MARGIN);
-    else
-      x_red -= (comp_width + BIG_MARGIN);
-
-    // background
-    ctx.fillStyle = (on_pitch) ? '#000000' : 'rgba(0, 0, 0, 0.5)';
-    ctx.fillRect(x_render, Y_COMP, comp_width, 2 * NAME_MARGIN + 2 * TEAM_RAD);
-
-    // team circle
-    ctx.fillStyle = (team == 0) ? 'blue' : 'red';
-    ctx.beginPath();
-    ctx.arc(x_render + NAME_MARGIN + TEAM_RAD, Y_COMP + NAME_MARGIN + TEAM_RAD, TEAM_RAD, 0, 2 * Math.PI);
-    ctx.fill();
-
-    // player name
-    ctx.font = "15px Courier New, monospace";
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText(short_name, x_render + 2 * NAME_MARGIN + 2 * TEAM_RAD, Y_COMP + 2.5 * NAME_MARGIN);
-  } );
+  draw_players( player_list );
+  draw_bodies( entity_list );
 }
 
 document.addEventListener('keydown', (evt) => {
@@ -247,9 +273,9 @@ document.addEventListener('keyup', (evt) => {
 });
 
 
-let socket = io();
+socket = io();
 
-let game_state = {
+game_state = {
   'round_info': {
       'score': [0, 0],
       'in_play': false,
@@ -270,11 +296,11 @@ socket.on('gamestate', (new_state) => {
   game_state = new_state;
 });
 
-let form = document.getElementById('join');
-let name_input = document.getElementById('name');
-let team_input = document.getElementById('team');
-let spectate_input = document.getElementById('spectate');
-let lobby_button = document.getElementById('lobby');
+form = document.getElementById('join');
+name_input = document.getElementById('name');
+team_input = document.getElementById('team');
+spectate_input = document.getElementById('spectate');
+lobby_button = document.getElementById('lobby');
 
 spectate_input.addEventListener('click', (evt) => {
   form.classList.add("form_acc");
@@ -303,15 +329,15 @@ socket.on('request_deny', (evt) => {
   form.classList.add("form_deny");
 });
 
-let MIN_REFRESH = 1000 / 50; // ms
+MIN_REFRESH = 1000 / 50; // ms
 
-let done = false;
-let prev_timestamp = undefined;
+done = false;
+prev_timestamp = undefined;
 function step( timestamp ) {
   if( !prev_timestamp )
     prev_timestamp = timestamp - (MIN_REFRESH+1);
 
-  let delta = timestamp - prev_timestamp;
+  delta = timestamp - prev_timestamp;
 
   if( delta >= MIN_REFRESH ){
     FPS = 1000 / delta;
